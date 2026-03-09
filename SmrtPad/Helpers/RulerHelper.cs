bdfbb56fc4b5c6328fbcd8c143d6963371ac6248
John Donnelly <54213031+John-Donnelly@users.noreply.github.com>
2026-03-09 22:56:57 +0000

    feat: add Points and Picas measurement units to RulerHelper

--- SmrtPad/Helpers/RulerHelper.cs
+++ SmrtPad/Helpers/RulerHelper.cs
@@ -7,21 +7,34 @@ namespace SmrtPad.Helpers
     {
         private const double ScreenDpi = 96.0;
         private const double CmPerInch = 2.54;
+        private const double PtPerInch = 72.0;
+        private const double PicaPerInch = 6.0;  // 1 pica = 12 pt; 72 pt/in ÷ 12 = 6 pica/in
 
         /// <summary>
         /// Calculates the number of pixels per ruler unit at the given zoom level.
         /// </summary>
-        /// <param name="rulerUnits">"cm" for centimeters, anything else for inches.</param>
+        /// <param name="rulerUnits">"cm", "pt", "pc", or anything else for inches.</param>
         /// <param name="zoomPercent">Zoom percentage (e.g. 100.0 for 100%).</param>
-        /// <param name="unitLabel">Returns "cm" or "in" based on the ruler unit setting.</param>
+        /// <param name="unitLabel">Returns the short unit label.</param>
         /// <returns>Pixels per ruler unit, scaled by zoom.</returns>
         public static double GetPixelsPerUnit(string rulerUnits, double zoomPercent, out string unitLabel)
         {
-            bool useCm = rulerUnits == "cm";
-            unitLabel = useCm ? "cm" : "in";
-            double basePixels = useCm ? ScreenDpi / CmPerInch : ScreenDpi;
             double scale = zoomPercent / 100.0;
-            return basePixels * scale;
+            switch (rulerUnits)
+            {
+                case "cm":
+                    unitLabel = "cm";
+                    return (ScreenDpi / CmPerInch) * scale;
+                case "pt":
+                    unitLabel = "pt";
+                    return (ScreenDpi / PtPerInch) * scale;
+                case "pc":
+                    unitLabel = "pc";
+                    return (ScreenDpi / PicaPerInch) * scale;
+                default:
+                    unitLabel = "in";
+                    return ScreenDpi * scale;
+            }
         }
     }
 }
